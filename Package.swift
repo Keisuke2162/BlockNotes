68f085b162d796f38ebcc3d1869dffada09304ff
Keisuke2162 <applabo.kei@gmail.com>
2024-07-14 00:16:55 +0900

    色々設定

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.10
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -8,6 +8,7 @@ let package = Package(
     platforms: [.iOS(.v17),],
     products: [
         .library(name: "BlockNotes", targets: ["BlockNotes"]),
+        .library(name: "HomeFeature", targets: ["HomeFeature"]),
     ],
     dependencies: [
       .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.11.1"),
@@ -15,7 +16,10 @@ let package = Package(
     targets: [
         .target(name: "BlockNotes"),
         .target(name: "Entities"),
-        .target(name: "HomeFeature", dependencies: ["Entities"]),
+        .target(name: "HomeFeature", dependencies: [
+          "Entities",
+          .product(name: "ComposableArchitecture", package: "swift-composable-architecture"),
+        ]),
         .testTarget(name: "BlockNotesTests", dependencies: ["BlockNotes"]),
     ]
 )
---
+++ Sources/HomeFeature/BlockItemView.swift
@@ -0,0 +1,23 @@
+//
+//  BlockItemView.swift
+//
+//
+//  Created by Kei on 2024/07/13.
+//
+
+import ComposableArchitecture
+import Foundation
+import SwiftUI
+
+struct BlockItemView: View {
+  public init() {
+  }
+
+  var body: some View {
+    Text("test")
+  }
+}
+
+#Preview {
+  BlockItemView()
+}
