d1975c06ba7df8840077e1442f15e2ac50e53919
Keisuke2162 <applabo.kei@gmail.com>
2024-07-13 20:23:42 +0900

    Initial Commit

---
+++ BlockNotes/BlockNotesApp.swift
@@ -0,0 +1,18 @@
+//
+//  BlockNotesApp.swift
+//  BlockNotes
+//
+//  Created by Kei on 2024/07/13.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BlockNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BlockNotes/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  BlockNotes
+//
+//  Created by Kei on 2024/07/13.
+//
+
+import SwiftUI
+import SwiftData
+
+struct ContentView: View {
+    var body: some View {
+      Text("Content")
+    }
+}
+
+#Preview {
+    ContentView()
+}
