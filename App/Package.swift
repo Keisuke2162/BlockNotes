9df76083ec528ed466ae5776ff250b087bd079a1
Keisuke2162 <applabo.kei@gmail.com>
2024-07-13 22:28:24 +0900

    SPM化

---
+++ App/Package.swift
@@ -0,0 +1,15 @@
+//
+//  Package.swift
+//
+//
+//  Created by Kei on 2024/07/13.
+//
+
+import PackageDescription
+
+let package = Package(
+  name: "",
+  products: [],
+  dependencies: [],
+  targets: []
+)
