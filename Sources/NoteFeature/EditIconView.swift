d53769a362fc5add716dc092a2159b32b943b99f
Keisuke2162 <applabo.kei@gmail.com>
2024-07-16 02:06:05 +0900

    add file

---
+++ Sources/NoteFeature/EditIconView.swift
@@ -0,0 +1,18 @@
+//
+//  EditIconView.swift
+//  
+//
+//  Created by Kei on 2024/07/16.
+//
+
+import Entities
+import SwiftUI
+
+public struct EditIconView: View {
+  
+  public init() {}
+
+  public var body: some View {
+    Text("test")
+  }
+}
