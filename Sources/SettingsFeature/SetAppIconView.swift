72fb0d6f933b551d51b9c916379ef8b9b93bbb84
Keisuke2162 <applabo.kei@gmail.com>
2024-10-17 15:59:48 +0900

    アイコン追加

---
+++ Sources/SettingsFeature/SetAppIconView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  BlockNotes
+//
+//  Created by Kei on 2024/10/17.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
